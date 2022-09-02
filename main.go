a10f5dfad3739c5dc42f645399b72634835bfc8e
Elizabeth Sedrak <elizabeth.sedrak94@gmail.com>
2022-09-02 18:22:41 +1000

    create a reader for the terminal

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+// create a reader - for the terminal
+func createCocktail() cocktail {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Cocktail name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	c := oneCocktail(name)
+	fmt.Println("Created a cocktail: ", c.name)
+
+	return c
+}
+
 func main() {
-	newCocktail := oneCocktail("Margarita")
+	myCocktail := createCocktail()
 
-	newCocktail.addIngredients("gin", 30)
-
-	fmt.Println(newCocktail.format())
+	fmt.Println(myCocktail)
 }
